d6a52557e28b63e8e57d411b9aedd386b5de4079
Mohamed Eleraqi <mohamed.8.eleraqi@gmail.com>
2024-11-18 18:21:54 +0200

    Create report.py

---
+++ report.py
@@ -0,0 +1,19 @@
+def main():
+    spacecraft={"name":""}
+    print(create_report(spacecraft))
+    
+    
+def create_report(spacecraft):
+    return f"""
+    ========= REPORT =========
+    
+    Name: TODO
+    Distance: TODO
+    
+    ==========================
+    """
+    
+    
+    
+    
+main()
