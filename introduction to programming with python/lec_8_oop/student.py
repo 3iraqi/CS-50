fc558045c161a02b32c42d56eda3640bb93ab213
Mohamed Eleraqi <mohamed.8.eleraqi@gmail.com>
2024-11-29 17:07:11 +0200

    new

--- introduction to programming with python/lec_8_oop/student.py
+++ introduction to programming with python/lec_8_oop/student.py
@@ -1,17 +1,47 @@
+import sys
+
 class Student:
-    def __init__(self,name,house):
+    def __init__(self,name,house,patronus):
+        if not name:
+            raise ValueError("Missing Name")
+        # if house not in ["sherbin","masr","egypt","cairo"]:
+        #     raise ValueError("Invalid House")
         self.name=name
         self.house=house
+        self.patronus=patronus
+        
+    def __str__(self):
+        return f"{self.name} from {self.house}"
+    
+    def charm(self):
+        match self.patronus:
+            case "stag":
+                return "🐴"
+            case "Otter":
+                return "🦦"
+            case "Jack Russell terrier":
+                return "🐶"
+            case _:
+                return "❕"
+    
+    
 
 def main():
     student =get_student()
-    print(f"{student.name} from {student.house}")
+    print("Expecto Patronum!")
+    print(student.charm())
+    # student.info()
+    
 
 def get_student():
     name=input("Name: ")
     house=input("House: ")
-    student=Student(name,house)
-    return student
+    patronus=input("Patronus: ")
+    return Student(name,house,patronus)
+    # try:
+    # except ValueError :
+        
+    
 
 if __name__ == "__main__":
     main()
