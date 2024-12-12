fa270119b5ff978aa52f8bbf6839660557214d16
Mohamed Eleraqi <mohamed.8.eleraqi@gmail.com>
2024-12-12 16:48:13 +0200

    new

--- introduction to programming with python/lec_4_libraries/generate.py
+++
@@ -1,9 +0,0 @@
-import random
-
-dice=random.choice([1,2,3,4,5,6])
-cards=["jack","queen","King"]
-random.shuffle(cards)
-print(cards)
-print(random.randint(1,9))
-
-print(dice)
--- introduction to programming with python/lec_4_libraries/name.py
+++
@@ -1,8 +0,0 @@
-import sys
-# sys,argv  -> Argument Vector : is a magic variable that can get 
-# all the words you write in command line before you hit enter.
-
-try:
-    print("hello, my name is",sys.argv[1])
-except IndexError:
-    print("oho who are you")
