f7607dff0cad79c78aab6d4a947d9e142be5b73b
Mohamed Eleraqi <mohamed.8.eleraqi@gmail.com>
2024-12-18 23:22:52 +0200

    new

--- introduction to programming with python/lec_5_w4 libraries/shorts/cards.py
+++ introduction to programming with python/lec_5_w4 libraries/shorts/cards.py
@@ -1,13 +1,17 @@
 """random module  To make random events"""
+
 import random
+
 cards = ["jack", "queen", "king"]
+
 
 def main():
     """main function"""
     random.seed(1)
-    print( random.choices(cards,weights=[75,20,5],k=2) ) # Sampling with replacement.
+    print(random.choices(cards, weights=[75, 20, 5], k=2))  # Sampling with replacement.
     # weights -> the probability of each card of the list of cards.
     # print( random.sample(cards,k=2) ) # Sampling without replacement
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     main()
